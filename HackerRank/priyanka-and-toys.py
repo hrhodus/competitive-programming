101b55ff1924b3f2524fd66d4e0ced1d10787d36
hrhodus <hrhodus5@gmail.com>
2017-09-28 09:03:15 -0400

    find the number of  inputs between initial int and 0

--- HackerRank/priyanka-and-toys.py
+++ HackerRank/priyanka-and-toys.py
@@ -1,15 +1,15 @@
 ls = [0]*10001
 length = input()
-assert 1 <= length <= 10**5
+
 costs = map(int,raw_input().split())
 for i in costs:
     ls[i] += 1
-    assert(0<= i <= 10**4)
-i = 0
+
+search = 0
 ans = 0
-while (i < len(ls)):
-    if ls[i] > 0:
+while (len(ls) > search):
+    if ls[search] > 0:
         ans +=1
-        i +=4
-    i += 1
-print ans    
+        search +=4
+    search += 1
+print ans
